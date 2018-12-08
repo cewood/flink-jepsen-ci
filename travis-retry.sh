75d7e21c33501e086787e5bef663d94728ef8a86
Gary Yao <gary@data-artisans.com>
2018-12-08 11:00:14 +0100

    fixup

--- travis-retry.sh
+++ travis-retry.sh
@@ -1,3 +1,6 @@
+# Copied from
+# https://github.com/travis-ci/travis-build/blob/4f580b238530108cdd08719c326cd571d4e7b99f/lib/travis/build/bash/travis_retry.bash
+
 travis_retry() {
 	local result=0
 	local count=1
@@ -11,7 +14,8 @@ travis_retry() {
 	done
 
 	[[ "${count}" -gt 3 ]] && {
-	echo -e "\\n${ANSI_RED}The command \"${*}\" failed 3 times.${ANSI_RESET}\\n" >&2
+		echo -e "\\n${ANSI_RED}The command \"${*}\" failed 3 times.${ANSI_RESET}\\n" >&2
+	}
 
 	return "${result}"
 }
